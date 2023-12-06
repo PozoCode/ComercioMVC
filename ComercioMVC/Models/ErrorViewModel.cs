0d322f0433f0e9b3968f63d15bf27d02c0737299
CristianPozo <35204280+PozoCode@users.noreply.github.com>
2023-12-05 21:00:27 -0500

    Creación del Proyecto

---
+++ ComercioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComercioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
